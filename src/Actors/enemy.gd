a9d419256a486676b536879aa707b15f95561499
vb <udithvb@gmail.com>
2023-08-07 18:30:40 +0530

    enemy dies if player stomps enemy

--- src/Actors/enemy.gd
+++ src/Actors/enemy.gd
@@ -4,6 +4,15 @@ extends "res://src/Actors/Actor.gd"
 
 func _ready() -> void:
 	_velocity.x = -speed.x
+	
+	set_physics_process(true)
+
+func _on_stompdetector_body_entered(body: Node) -> void:
+	if body.global_position.y > get_node("stompdetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+ 
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
@@ -11,3 +20,4 @@ func _physics_process(delta: float) -> void:
 		_velocity.x *= -1
 	
 	_velocity.y = move_and_slide(_velocity,FLOOR_NORMAL).y 
+
